cc3f848d176ba739971705fac5503c5db01bc828
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-04-24 22:08:38 +0530

    Initial project creation using start.spring.io

---
+++ learn-and-apply-rabbitmq-using-springboot/src/main/java/com/techstack/rabbitmq/LearnAndApplyRabbitmqUsingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.techstack.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAndApplyRabbitmqUsingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAndApplyRabbitmqUsingSpringbootApplication.class, args);
+	}
+
+}
---
+++ learn-and-apply-rabbitmq-using-springboot/src/test/java/com/techstack/rabbitmq/LearnAndApplyRabbitmqUsingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstack.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAndApplyRabbitmqUsingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
