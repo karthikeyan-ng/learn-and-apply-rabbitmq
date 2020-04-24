28f96793ed6131238da21dda84458eafc1f17be0
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-04-24 23:27:18 +0530

    How to consume Message which contains byte[] array?

--- learn-and-apply-rabbitmq-using-springboot/src/main/java/com/techstack/rabbitmq/consumer/MessageConsumer.java
+++ learn-and-apply-rabbitmq-using-springboot/src/main/java/com/techstack/rabbitmq/consumer/MessageConsumer.java
@@ -5,7 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
 import static com.techstack.rabbitmq.MessageConfigInfo.MOBILE_QUEUE;
+import static com.techstack.rabbitmq.MessageConfigInfo.TV_QUEUE;
 
 @Slf4j
 @Service
@@ -19,6 +25,22 @@ public class MessageConsumer {
      */
     @RabbitListener(queues = MOBILE_QUEUE)
     public void getMessageFromMobileQueue(Person person) {
+
+        log.info("Message Received : {}", person);
+
+    }
+
+    @RabbitListener(queues = TV_QUEUE)
+    public void getMessageFromTvQueue(byte[] message) throws IOException, ClassNotFoundException {
+
+        /**
+         * Convert byte[] array message to Object type
+         */
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(message);
+        ObjectInput objectInput = new ObjectInputStream(byteArrayInputStream);
+        Person person = (Person) objectInput.readObject();
+        byteArrayInputStream.close();
+        objectInput.close();
 
         log.info("Message Received : {}", person);
 
