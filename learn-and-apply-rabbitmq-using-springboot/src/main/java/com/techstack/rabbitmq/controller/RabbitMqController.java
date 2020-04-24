4b6fa235061b2aeb92dd1d3fbccabfbae48c7e26
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-04-24 23:04:42 +0530

    Created a Message Producer using RabbitTemplate covered HeaderExchange.

--- learn-and-apply-rabbitmq-using-springboot/src/main/java/com/techstack/rabbitmq/controller/RabbitMqController.java
+++ learn-and-apply-rabbitmq-using-springboot/src/main/java/com/techstack/rabbitmq/controller/RabbitMqController.java
@@ -2,14 +2,22 @@ package com.techstack.rabbitmq.controller;
 
 import com.techstack.rabbitmq.model.Person;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
 import static com.techstack.rabbitmq.MessageConfigInfo.DIRECT_EXCHANGE_NAME;
 import static com.techstack.rabbitmq.MessageConfigInfo.FANOUT_EXCHANGE_NAME;
+import static com.techstack.rabbitmq.MessageConfigInfo.HEADERS_EXCHANGE_NAME;
 import static com.techstack.rabbitmq.MessageConfigInfo.MOBILE_QUEUE;
 import static com.techstack.rabbitmq.MessageConfigInfo.ROUTING_KEY_FOR_MOBILE;
 import static com.techstack.rabbitmq.MessageConfigInfo.TOPIC_EXCHANGE_NAME;
@@ -57,6 +65,37 @@ public class RabbitMqController {
          * By using below method, we can send a message to "Topic Exchange" using "Routing Key"
          */
         rabbitTemplate.convertAndSend(TOPIC_EXCHANGE_NAME, "tv.mobile.ac", person);
+
+        return "success";
+    }
+
+    @GetMapping("/test/headers-exchange/{name}")
+    public String testHeadersExchange(@PathVariable String name) throws IOException {
+
+        Person person = new Person(1L, name);
+
+        /**
+         * Java way of converting Object to Bytes
+         */
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutput objectOutput = new ObjectOutputStream(baos);
+        objectOutput.writeObject(person);
+        objectOutput.flush();
+        objectOutput.close();
+
+        byte[] byteMessage = baos.toByteArray();
+        baos.close();
+
+        /**
+         * Prepare a message
+         */
+        Message message = MessageBuilder
+                            .withBody(byteMessage)
+                            .setHeader("item1", "mobile")
+                            .setHeader("item2", "television")
+                            .build();
+
+        rabbitTemplate.send(HEADERS_EXCHANGE_NAME, "", message);
 
         return "success";
     }
